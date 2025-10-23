890f305d70cf6335c3c436f66b8cbb2afebad3cf
komantuhamid <komantuhamid@gmail.com>
2025-10-23 03:59:09 +0100

    Update TowerGame.js

--- app/components/TowerGame/TowerGame.js
+++ app/components/TowerGame/TowerGame.js
@@ -1,1 +1,26 @@
 
+'use client'
+
+import { useEffect, useRef } from 'react'
+
+export default function TowerGame() {
+  const gameRef = useRef(null)
+  
+  useEffect(() => {
+    // استيراد اللعبة
+    import('./game/index.js').then((gameModule) => {
+      // شغل اللعبة
+      if (gameModule.default) {
+        gameModule.default(gameRef.current)
+      }
+    })
+  }, [])
+  
+  return (
+    <div 
+      ref={gameRef} 
+      id="game-container"
+      style={{ width: '100%', height: '100vh' }}
+    />
+  )
+}
