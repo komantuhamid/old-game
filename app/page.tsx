fe2f3d5b3612cc4bae15efefe89c508d914e64a8
komantuhamid <komantuhamid@gmail.com>
2025-10-23 04:16:01 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import TowerGame from './components/TowerGame/TowerGame'
 
 export default function Home() {
-  return <TowerGame />
+  return (
+    <main style={{ 
+      width: '100%', 
+      height: '100vh',
+      margin: 0,
+      padding: 0,
+      overflow: 'hidden'
+    }}>
+      <TowerGame />
+    </main>
+  )
 }
